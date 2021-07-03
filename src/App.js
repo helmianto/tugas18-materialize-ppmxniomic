4a717dbe319d1c64f98db59e5d72222e4dfde84d
Helmianto <helmianto15@gmail.com>
2021-07-03 23:41:54 +0700

    intial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,202 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import { Caption, Slider, Slide, Row, Col, Card, Icon, CardTitle, SideNav, SideNavItem, Button, Pagination } from 'react-materialize'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+class App extends Component {
+  render(){
+    return (
+      <div>
+        
+        <br />
+        <Slider
+          fullscreen={false}
+          options={{
+            duration: 500,
+            height: 400,
+            indicators: true,
+            interval: 6000
+          }}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+          <Slide image={<img alt="" src="https://s-ec.bstatic.com/images/hotel/max1024x768/106/106114417.jpg"/>}>
+            <Caption placement="center">
+              <h3>
+                Pantai
+              </h3>
+              <h5 className="light grey-text text-lighten-3">
+                Dapatkan Kesempatan Jalan - Jalan Ke Pantai Dengan Membeli Sebuah Product
+              </h5>
+            </Caption>
+          </Slide>
+          <Slide image={<img alt="" src="https://cdn.pixabay.com/photo/2015/12/08/00/26/cityscape-1081704_960_720.jpg"/>}>
+            <Caption placement="left">
+              <h3>
+                Left Aligned Caption
+              </h3>
+              <h5 className="light grey-text text-lighten-3">
+                Here's our small slogan.
+              </h5>
+            </Caption>
+          </Slide>
+          <Slide image={<img alt="" src="https://photos.mandarinoriental.com/is/image/MandarinOriental/paris-2017-home?wid=2880&hei=1280&fmt=jpeg&crop=9,336,2699,1200&anchor=1358,936&qlt=75,0&fit=wrap&op_sharpen=0&resMode=sharp2&op_usm=0,0,0,0&iccEmbed=0&printRes=72"/>}>
+            <Caption placement="right">
+              <h3>
+                Right Aligned Caption
+              </h3>
+              <h5 className="light grey-text text-lighten-3">
+                Here's our small slogan.
+              </h5>
+            </Caption>
+          </Slide>
+        </Slider>
+        <br />
+        <p>Hot List</p>
+        <Row>
+          <Col
+            m={3}
+            s={12}
+          >
+            <Card
+              actions={[
+                ""
+              ]}
+              closeIcon={<Icon>close</Icon>}
+              header={<CardTitle image="https://ecs7.tokopedia.net/img/cache/300/attachment/2018/8/15/25749302/25749302_274a89a6-ea41-4a3a-ac6b-ce2881016369.jpg">Audio</CardTitle>}
+              revealIcon={<Icon>more_vert</Icon>}
+            >
+              Audio Technica - Mulai Dari <span style={{backgroundColor: 'orange'}}>Rp 910 rb</span>
+            </Card>
+          </Col>
+          <Col
+            m={3}
+            s={12}
+          >
+            <Card
+              actions={[
+                ""
+              ]}
+              closeIcon={<Icon>close</Icon>}
+              header={<CardTitle image="https://ecs7.tokopedia.net/img/cache/300/attachment/2019/1/16/25287362/25287362_a79f0aa6-cf1f-484f-b1a8-83993245cd6f.jpg">Handphone</CardTitle>}
+              revealIcon={<Icon>more_vert</Icon>}
+            >
+              Handphone Samsung - Mulai Dari <span style={{backgroundColor: 'orange'}}>Rp 910 rb</span>
+            </Card>
+          </Col>
+          <Col
+            m={3}
+            s={12}
+          >
+            <Card
+              actions={[
+                ""
+              ]}
+              closeIcon={<Icon>close</Icon>}
+              header={<CardTitle image="https://ecs7.tokopedia.net/img/cache/300/attachment/2018/6/26/28201197/28201197_856195f2-b0ba-4711-8200-fd95beaf1df5.jpg"></CardTitle>}
+              revealIcon={<Icon>more_vert</Icon>}
+            >
+              Squishy - Mulai Dari <span style={{backgroundColor: 'orange'}}>Rp 910 rb</span>
+            </Card>
+          </Col>
+          <Col
+            m={3}
+            s={12}
+          >
+            <Card
+              actions={[
+                ""
+              ]}
+              closeIcon={<Icon>close</Icon>}
+              header={<CardTitle image="https://ecs7.tokopedia.net/img/cache/300/attachment/2018/4/23/28201197/28201197_0383072a-1766-453c-951a-8b53a719cff0.jpg"></CardTitle>}
+              revealIcon={<Icon>more_vert</Icon>}
+            >
+              Koleksi The Avanger - Mulai Dari <span style={{backgroundColor: 'orange'}}>Rp 910 rb</span>
+            </Card>
+          </Col>
+        </Row>
+        <Pagination items={10} activePage={2} maxButtons={8} />
+        <style>
+            {`
+              #root > div > div {
+                z-index: 99999 !important;
+              }
+          `}
+        </style>
+        <SideNav
+          id="SideNav-10"
+          options={{
+            draggable: true
+          }}
+          trigger={<Button node="button">SIDE NAVIGATION</Button>}
+        >
+          <SideNavItem
+            user={{
+              background: 'https://placeimg.com/640/480/tech',
+              email: 'test@gmail.com',
+              image: 'static/media/react-materialize-logo.824c6ea3.svg',
+              name: 'Satria Darmawan'
+            }}
+            userView
+          />
+          <SideNavItem
+            href="#!icon"
+            icon={<Icon>person</Icon>}
+          >
+            Profil Saya
+          </SideNavItem>
+          <SideNavItem
+            href="#!icon"
+            icon={<Icon>help</Icon>}
+          >
+            Hubungi Kami
+          </SideNavItem>
+          <SideNavItem divider />
+          <SideNavItem subheader>
+            Product
+          </SideNavItem>
+          <SideNavItem
+            href="#!third"
+            waves
+          >
+            Kesehatan
+          </SideNavItem>
+          <SideNavItem
+            href="#!third"
+            waves
+          >
+            Komputer & Aksesoris
+          </SideNavItem>
+          <SideNavItem
+            href="#!third"
+            waves
+          >
+            Gaming
+          </SideNavItem>
+          <SideNavItem
+            href="#!third"
+            waves
+          >
+            Kamera
+          </SideNavItem>
+          <SideNavItem
+            href="#!third"
+            waves
+          >
+            Olahraga
+          </SideNavItem>
+          <SideNavItem
+            href="#!third"
+            waves
+          >
+            Fashion Pria
+          </SideNavItem>
+          <SideNavItem
+            href="#!third"
+            waves
+          >
+            Fashion Wanita
+          </SideNavItem>
+        </SideNav>
+      </div>
+    );
+  }
 }
 
 export default App;
